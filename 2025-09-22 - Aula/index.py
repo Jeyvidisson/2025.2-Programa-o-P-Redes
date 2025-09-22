234477ea04af75973ec5a8ee7f84536b7194c0c2
Jeyvidisson <fbj86618@gmail.com>
2025-09-22 20:30:46 -0300

    Printzim

---
+++ 2025-09-22 - Aula/index.py
@@ -0,0 +1,16 @@
+
+#AAAAAAAAAAAAAAAAAAAAAAAAAA
+
+
+nome = "Jemersson Félix";
+idade = 19.255555555;
+
+print(f'Olá, {nome}, você tem ----> {idade} <--- anos te idade');
+
+print(f'Olá, {nome}, você tem ----> {idade} <--- anos te idade');
+
+print(f'Olá, {nome}, você tem ----> {idade} <--- anos te idade');
+
+print(f'Olá, {nome}, você tem ----> {idade} <--- anos te idade');
+
+print(f'Olá, {nome}, você tem ----> {idade} <--- anos te idade');
